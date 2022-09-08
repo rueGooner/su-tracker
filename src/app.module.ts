e9f5c767871eba9d5dfacb37d88f75a6c17a7d84
rue-oplo <rueben.cumberbatch@myoplo.co.uk>
2022-09-08 21:06:10 +0100

    create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SeederModule } from './seeder/seeder.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [SeederModule, PrismaModule],
+  imports: [SeederModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
