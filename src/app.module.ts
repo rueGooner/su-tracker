dec178b3b46b5c7f2c4c9706177399c3a5ee0330
rue-oplo <rueben.cumberbatch@myoplo.co.uk>
2022-09-08 13:52:48 +0100

    set up the prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SeederModule } from './seeder/seeder.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [SeederModule],
+  imports: [SeederModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
